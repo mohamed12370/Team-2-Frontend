19a403e91897d3f06bdbdeb01ecb3bca73591b22
mohamed12370 <mohamedmostafa12370@gmail.com>
2023-07-02 18:00:50 +0300

    create new user page finsh desing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StudentPortalPage from './Pages/StudentePortalPage/StudentPortalPage';
 import HomePage from './Pages/Home/HomePage';
 import ExamResultPage from './Pages/ExamResult/ExamResultPage';
+import AdminCreateUserPage from './Pages/Admin/AdminCreateUserPage/AdminCreateUserPage';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/studentportal" element={<StudentPortalPage />} />
           <Route path="/examresult" element={<ExamResultPage />} />
+          {/* Admin Route */}
+          <Route path="/createuser" element={<AdminCreateUserPage />} />
         </Routes>
       </BrowserRouter>
     </>
